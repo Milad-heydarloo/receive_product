7676aca51fa5707f6175693bb189d7ff9b82272a
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-08-07 16:45:50 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# receive_product
+ https://saaterco.com CRM project of Sater business development system
---
+++ android/app/src/main/kotlin/com/example/receive_product/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.receive_product
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
