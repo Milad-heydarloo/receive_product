44ba5262ae49833509e3290c5ab85a28652bd29c
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-08-07 17:37:00 +0330

    change

--- lib/Getx/PagesOrderProduct/PagesOP.dart
+++ lib/Getx/PagesOrderProduct/PagesOP.dart
@@ -2,13 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:receive_product/Getx/Drawer/DrawerController.dart';
 import 'package:receive_product/Getx/Drawer/MyDrawer.dart';
+import 'package:receive_product/Getx/auth_controller.dart';
+import 'package:receive_product/Getx/routes.dart';
 // Import the updated controller
 
-class OP extends StatelessWidget {
+
+
+class OP extends StatefulWidget {
+  const OP({Key? key}) : super(key: key);
+
+  @override
+  State<OP> createState() => _OPState();
+}
+
+class _OPState extends State<OP> {
+  final MyDrawerController drawerController = Get.find<MyDrawerController>(); // Get the controller instance
+  final AuthController authController = Get.find<AuthController>();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+ // ini();
+  }
   @override
   Widget build(BuildContext context) {
-    final MyDrawerController drawerController = Get.find<MyDrawerController>(); // Get the controller instance
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Empty Page'),
@@ -22,4 +39,20 @@ class OP extends StatelessWidget {
       drawer: MyDrawer(), // Pass the drawer with the updated controller
     );
   }
+
+  // void ini() async{
+  //   await Future.delayed(Duration(seconds: 2));
+  //   bool user = await authController.checkVerificationStatus();
+  //
+  //   if (user) {
+  //     //Get.offAllNamed(Routes.home);
+  //     print('ok');
+  //   } else {
+  //     authController.clearUser();
+  //     Get.offAllNamed(Routes.login);
+  //   }
+  //
+  // }
 }
+
+
